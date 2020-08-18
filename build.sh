f552f5c0c8d92e0f9c5d7927474267b2d67b24b6
Jason Kuruzovich <jkuruzovich@gmail.com>
2020-08-18 14:01:41 -0400

    push to public

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-book=introml
+book=introml/
 #cd scripts && python convert.py
-jupyter-book toc ./$book
-jupyter-book build $book && cd $book
+#jupyter-book toc ./$book
+jupyter-book build $book
